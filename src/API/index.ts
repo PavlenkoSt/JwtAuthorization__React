a25a4302479de96644719e7ac1085b9f57ed1acf
Stanislav <verbatim8872@gmail.com>
2021-10-12 21:26:14 +0300

    axios interceptor request

--- src/API/index.ts
+++ src/API/index.ts
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios, { AxiosRequestConfig } from "axios"
 
 const API_URL = 'http://localhost:5000/api'
 
@@ -6,3 +6,12 @@ const $api = axios.create({
     withCredentials: true,
     baseURL: API_URL
 })
+
+$api.interceptors.request.use((config: AxiosRequestConfig) => {
+    if(config.headers){
+        config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+    }
+    return config
+})
+
+export default $api
